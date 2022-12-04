bf4cdd99754e496a1c41981073ff61a2424a2294
Justin Silverman <jsilve24@gmail.com>
2022-12-04 16:49:35 -0500

    working

--- ess-breakerofchains.R
+++ ess-breakerofchains.R
@@ -1,18 +1,16 @@
-##' .. content for \description{} (no empty lines) ..
+##' Wrapper around breakerofchains::get_broken_chain()
 ##'
-##' .. content for \details{} ..
-##' @title 
-##' @param doc_lines 
-##' @param doc_cursor_line 
-##' @return 
+##' This is a small wrapper intended to adapt MilesMcBains breakerofchains library for use with
+##' emacs and ESS.
+##' @param doc_lines an R vector of strings each represneting a line in the file with the chain
+##' @param doc_cursor_line the current line the cursor is on
+##' @return .chain an object storing the output of the chain unless assign_result=FALSE
 ##' @author Justin Silverman
 ##' @import breakerofchains
 ##' @export
 ess_break_chains <- function(doc_lines, doc_cursor_line,
                              print_result=TRUE, assign_result=TRUE) {
 
-  print(doc_lines)
-  print(doc_cursor_line)
   truncated_context <-
     breakerofchains:::truncate_to_chunk_boundary(doc_lines, doc_cursor_line)
 
--- ess-breakerofchains.el
+++ ess-breakerofchains.el
@@ -2,10 +2,10 @@
 
 ;; Author: Justin Silverman (justinsilverman@psu.edu)
 ;; Maintainer: Justin Silverman (justinsilverman@psu.edu)
-;; Version: version
+;; Version: 0.1
 ;; Package-Requires: (dependencies)
-;; Homepage: homepage
-;; Keywords: keywords
+;; Homepage: https://github.com/jsilve24/ess-breakerofchains
+;; Keywords: R ESS 
 
 
 ;; This file is not part of GNU Emacs
@@ -24,34 +24,36 @@
 ;;; Commentary:
 
 ;; commentary
+;; See README.org
 
 ;;; Code:
 
 (require 'ess)
 
-(defun ess-boc-break-chain (&optional print-result assign-result)
-    "Elisp port of break_chains."
-  (interactive)
+(defun ess-boc-break-chain (&optional dont-print-result dont-assign-result)
+    "Elisp port of break_chains. Do not print result if universal-argument passed."
+  (interactive "P")
   (let* ((doc-lines (ess-boc--get-doc-lines))
 	 (doc-cursor-line (line-number-at-pos))
 	 (proc (ess-get-process))
-	 (print-result-p (if print-result "TRUE" "FALSE"))
-	 (assign-result-p (if assign-result "TRUE" "FALSE"))
+	 (dont-print-result-p (if dont-print-result  "FALSE" "TRUE"))
+	 (dont-assign-result-p (if dont-assign-result "FALSE" "TRUE"))
 	 ;; doc-lines to formated R vector
 	 (doc-lines-r (ess-boc--parse-doc-lines doc-lines)))
     ;; make sure breakerofchains and ess-breakerofchains is installed
-    ;; ...
+    ;; TODO
     ;; pass to R for further processing
+    (message (format "%s" dont-assign-result-p))
     (ess-send-string proc (format "ess_break_chains(%s, %s, %s, %s)"
 				  doc-lines-r
 				  doc-cursor-line
-				  print-result-p
-				  assign-result-p)))
+				  dont-print-result-p
+				  dont-assign-result-p)))
   )
 
 (defun ess-boc--get-doc-lines ()
     "Return list of buffer lines from line 1 to current line."
-    (split-string (buffer-string) "\n" t)) ; t omits nulls
+    (split-string (buffer-string) "\n" nil)) ; t omits nulls
 
 (defun ess-boc--parse-doc-lines (doc-lines)
     "Parse output of ESS-BOC--GET-DOC-LINES and format into string
@@ -60,7 +62,16 @@ formated as R vector."
 			     doc-lines ",")))
       (format "c(%s)" lines)))
 
+(defun ess-boc-install-r-package ()
+  "Install the ESSBreakerOfChains R Package if not already installed."
+  (interactive)
+  (let* ((dir (file-name-directory (buffer-file-name))))
+    (ess-r-package-eval-linewise
+     (format  "devtools::install('%s')\n" dir) "Installing %s")))
+
 (provide 'ess-breakerofchains)
 
 ;;; ess-breakerofchains.el ends here
+
+
 
